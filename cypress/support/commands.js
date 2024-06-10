ef860b748df476114cce5dec8f92003eb5726384
Pablo Sebastian Rivera <psrivera@MacBook-Air-de-Pablo.local>
2024-06-10 12:51:33 +0200

    Remove commands

--- cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-import homeRenfe from "../pages/homeRenfe"
-
-Cypress.Commands.add('visitRenfe', () => {
-  cy.fixture('dataRenfe').then((data) => {
-    cy.visit(data.Home)
-    cy.title().should('include', data.Title)
-    homeRenfe.clickOn(homeRenfe.selectors.acceptCookies)
-  })
-}) 
